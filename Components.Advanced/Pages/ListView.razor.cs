ff2bc3a5aafc3beff2be94edd40526e0da6f38d5
MattM <67703490+Matthew-Muir@users.noreply.github.com>
2022-04-17 21:57:37 -0700

    Creating Razor template components

---
+++ Components.Advanced/Pages/ListView.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace Components.Advanced.Pages
+{
+    public partial class ListView<TItem>
+    {
+        [Parameter]
+        public RenderFragment<TItem> ItemTemplate { get; set; } = default!;
+
+        [Parameter]
+        public IReadOnlyList<TItem> Items { get; set; } = default!;
+    }
+}
---
+++ Components.Advanced/Pages/ListView2.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace Components.Advanced.Pages
+{
+    public partial class ListView2<TItem>
+    {
+        [Parameter]
+        public RenderFragment<RenderFragment>? ListTemplate { get; set; }
+
+        [Parameter]
+        public RenderFragment<TItem> ItemTemplate { get; set; } = default!;
+
+        [Parameter]
+        public IReadOnlyList<TItem> Items { get; set; } = default!;
+    }
+}
