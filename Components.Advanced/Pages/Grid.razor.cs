c73c60823a41f42541654251f6b51d579edaeed1
MattM <67703490+Matthew-Muir@users.noreply.github.com>
2022-04-17 21:08:01 -0700

    Add project files.

---
+++ Components.Advanced/Pages/Grid.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace Components.Advanced.Pages
+{
+    public partial class Grid<TItem>
+    {
+        [Parameter]
+        public RenderFragment Header { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment<TItem> Row { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment Footer { get; set; } = default!;
+
+        [Parameter]
+        public IReadOnlyList<TItem> Items { get; set; } = default!;
+    }
+}
